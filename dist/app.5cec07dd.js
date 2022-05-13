c8f8319534bc4ef48200021219ec6903321e44d9
dengoyoon <engus927@naver.com>
2022-05-14 05:06:21 +0900

    배경 고정 구현

--- dist/app.5cec07dd.js
+++ dist/app.5cec07dd.js
@@ -961,7 +961,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51862" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61893" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
